d8f53f55c368aec596acae7f6977af57f03d3873
kristof1345 <kristofnemeth1345@gmail.com>
2023-03-19 21:16:19 +0100

    commit

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -4,10 +4,23 @@ import Header from "../components/Header";
 import BottomNav from "../components/BottomNav";
 import Post from "../components/Post";
 
+import { useAuthState } from "react-firebase-hooks/auth";
+
 const Dashboard = () => {
+  const [user] = useAuthState(auth);
   return (
     <div className="App">
       <Header />
+      <div className="stories">
+        <img src={user.photoURL} alt="photo" className="story" />
+      </div>
+      <div className="posts">
+        <div className="post"></div>
+        <div className="post"></div>
+        <div className="post"></div>
+        <div className="post"></div>
+        <div className="post"></div>
+      </div>
       <BottomNav />
     </div>
   );
