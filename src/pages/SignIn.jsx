d1d1b306db2cb463adf8d7101c70f841251d03ad
kristof1345 <kristofnemeth1345@gmail.com>
2023-03-23 20:37:22 +0100

    save

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,12 +1,59 @@
-import { app, auth } from "../firebaseConfig";
 import logo from "../../public/instagram_logo.png";
 
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
+import { v4 } from "uuid";
+
+import {
+  collection,
+  addDoc,
+  query,
+  doc,
+  updateDoc,
+  orderBy,
+} from "firebase/firestore";
+
+import { useAuthState } from "react-firebase-hooks/auth";
+
+import { useCollectionData } from "react-firebase-hooks/firestore";
+
+import { app, database, auth, storage } from "../firebaseConfig";
+
 const SignIn = () => {
+  const collectionRef = collection(database, "users");
+
   const signInWithGoogle = () => {
     const provider = new GoogleAuthProvider();
-    signInWithPopup(auth, provider);
+    signInWithPopup(auth, provider).then((x) => {
+      if (findOtherUsers(x.user.email)) {
+        addDoc(collectionRef, {
+          displayName: x.user.displayName,
+          email: x.user.email,
+          photoURL: x.user.photoURL,
+          uid: x.user.uid,
+          id: "",
+        }).then((docRef) => {
+          let docToUp = doc(database, "users", docRef.id);
+          updateDoc(docToUp, {
+            id: docRef.id,
+          }).catch((err) => console.log(err));
+        });
+      }
+    });
+  };
+
+  const findOtherUsers = (email) => {
+    const collectionRef = collection(database, "users");
+    const q = query(collectionRef, orderBy("email"));
+    const [users, loading, error] = useCollectionData(q);
+    let res = true;
+    users.map((user) => {
+      console.log(user);
+      if (user.eamil === email) {
+        res = false;
+      }
+    });
+    return res;
   };
 
   return (
