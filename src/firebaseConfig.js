dba3515a422ee46dd1de2978dc5ca47067e41b09
kristof1345 <kristofnemeth1345@gmail.com>
2023-04-20 19:26:10 +0200

    .

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -4,13 +4,7 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDfrHE3uB5LdW6rb5FbxdSVlBG3X_VqzT0",
-  authDomain: "instagram-clone2-eb5b2.firebaseapp.com",
-  projectId: "instagram-clone2-eb5b2",
-  storageBucket: "instagram-clone2-eb5b2.appspot.com",
-  messagingSenderId: "1090185974774",
-  appId: "1:1090185974774:web:f7b3ccfdcae42bc14e2dd7",
-  measurementId: "G-F16V8MED7T",
+  //Config specs
 };
 
 // Initialize Firebase
